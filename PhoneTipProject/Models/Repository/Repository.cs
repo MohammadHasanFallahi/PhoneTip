a5745d5ab88cd05a0ac77a91044d066a8d6cf739
Mohammad Fallahi <mohammad76fallahi.mf@gmail.com>
2023-01-01 22:49:57 +0330

    Add generic Repository To MyProject

---
+++ PhoneTipProject/Models/Repository/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using PhoneTipProject.Models;
+using PhoneTipProject.Models.Context;
+
+namespace PhoneTipProject.Models.Repository
+{
+    public class Repository<T> where T : class
+    {
+        private MyContext _myContext;
+        private DbSet<T> _dbset;
+
+        public Repository(MyContext context)
+        {
+            _myContext = context;
+            _dbset = _myContext.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> query = _dbset;
+            if (filter != null)
+            {
+                return query.Where(filter).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+
+        }
+
+        public virtual T Find(object id)
+        {
+            return _dbset.Find(id);
+        }
+
+        public virtual void Add(T Entity)
+        {
+            _dbset.Add(Entity);
+        }
+
+        public virtual void Remove(T Entity)
+        {
+            _dbset.Remove(Entity);
+        }
+
+        public virtual void Update(T Entity)
+        {
+            _myContext.Entry(Entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ PhoneTipProject/Models/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using PhoneTipProject.Models.Context;
+using PhoneTipProject.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhoneTipProject.Models.DataLayer;
+
+namespace PhoneTipProject.Models.UnitOfWork
+{
+    public class UnitOfWork : IDisposable
+    {
+        private MyContext context = new MyContext();
+        public Repository<PagesGroup> pagegroup { get => new Repository<PagesGroup>(context); }
+        public Repository<Pages> Pages { get => new Repository<Pages>(context); }
+        public Repository<PageComments> PageComments { get => new Repository<PageComments>(context); }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
