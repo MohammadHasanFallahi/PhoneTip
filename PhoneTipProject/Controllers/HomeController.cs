dbee6bf57efd81914ef689b44fea047fbd8da3ef
Mohammad Fallahi <mohammad76fallahi.mf@gmail.com>
2023-01-15 13:43:08 +0330

    add slider in index view

--- PhoneTipProject/Controllers/HomeController.cs
+++ PhoneTipProject/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PhoneTipProject.Models.UnitOfWork;
+using PhoneTipProject.Models.DataLayer;
 
 namespace PhoneTipProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UnitOfWork unitOfWork = new UnitOfWork();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,19 @@ namespace PhoneTipProject.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult ShowSlider()
+        {
+            IEnumerable<Pages> Pages_Slider = unitOfWork.Pages.GetAll().Where(x => x.ShowInSlider == true);
+            return PartialView(Pages_Slider);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            unitOfWork.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
