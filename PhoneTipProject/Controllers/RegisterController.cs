c280270bf66f8aed7b0657c20792962fe566cefd
Mohammad Fallahi <mohammad76fallahi.mf@gmail.com>
2023-01-20 19:37:45 +0330

    create Registersusers

---
+++ PhoneTipProject/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using PhoneTipProject.Models.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneTipProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UnitOfWork unitOfWork = new UnitOfWork();
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                unitOfWork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PhoneTipProject/ViewModels/RegistersViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhoneTipProject.ViewModels
+{
+    public class RegistersViewModel
+    {
+        [Display(Name ="نام کاربری")]
+        [Required(ErrorMessage ="لطفا نام کاربری را وارد کنید")]
+        public string UserName { get; set; }
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا ایمیل خود را وارد کنید")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Display(Name ="کلمه عبور")]
+        [Required(ErrorMessage ="لطفا کلمه عبور خود را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+        [Display(Name = "تکرار کلمه عبور")]
+        [Required(ErrorMessage = "لطفا تکرار کلمه عبور خود را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare("PassWord",ErrorMessage ="کلمه های عبور مغایرت دارند")]
+        public string RePassWord { get; set; }
+    }
+}
