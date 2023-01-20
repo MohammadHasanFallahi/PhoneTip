27dc5d9741369414b3eb8774ade9c0e1b997d561
Mohammad Fallahi <mohammad76fallahi.mf@gmail.com>
2023-01-20 18:30:25 +0330

    add new controller roles to adminpanel

---
+++ PhoneTipProject/Areas/AdminPanel/Controllers/RolesController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PhoneTipProject.Models.Context;
+using PhoneTipProject.Models.DataLayer;
+
+namespace PhoneTipProject.Areas.AdminPanel.Controllers
+{
+    public class RolesController : Controller
+    {
+        private MyContext db = new MyContext();
+        public ActionResult Index()
+        {
+            return View(db.Roles.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Roles roles = db.Roles.Find(id);
+            if (roles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roles);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RoleID,RoleTitel,RoleName")] Roles roles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(roles);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(roles);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Roles roles = db.Roles.Find(id);
+            if (roles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roles);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RoleID,RoleTitel,RoleName")] Roles roles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(roles).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(roles);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Roles roles = db.Roles.Find(id);
+            if (roles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roles);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Roles roles = db.Roles.Find(id);
+            db.Roles.Remove(roles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
