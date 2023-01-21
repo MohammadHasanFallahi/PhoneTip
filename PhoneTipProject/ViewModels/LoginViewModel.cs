9fc03cbde627e44a1ebb02ef6e20790f9b0694bd
Mohammad Fallahi <mohammad76fallahi.mf@gmail.com>
2023-01-21 22:56:22 +0330

    add loginviewmodel

---
+++ PhoneTipProject/ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhoneTipProject.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name ="ایمیل")]
+        [Required(ErrorMessage ="لطفا ایمیل خود را جهت ورود به سایت وارد کنید")]
+        [EmailAddress(ErrorMessage ="ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+        [Display(Name ="کلمه عبور")]
+        [Required(ErrorMessage ="لطفا کلمه عبور خود را جهت ورود به سایت وارد کنید")]
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+        [Display(Name ="مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
