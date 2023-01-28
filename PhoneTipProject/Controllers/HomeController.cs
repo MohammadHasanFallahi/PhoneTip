a956691c1ef88cd64451dfd49354acfa2eb28cad
Mohammad Fallahi <mohammad76fallahi.mf@gmail.com>
2023-01-28 23:02:17 +0330

    add action UsersControl

--- PhoneTipProject/Controllers/HomeController.cs
+++ PhoneTipProject/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using PhoneTipProject.Models.UnitOfWork;
 using PhoneTipProject.Models.DataLayer;
+using PhoneTipProject.ViewModels;
+using PhoneTipProject.Utilities.ExtensionMethods;
 
 namespace PhoneTipProject.Controllers
 {
@@ -37,6 +39,20 @@ namespace PhoneTipProject.Controllers
             return PartialView(Pages_Slider);
         }
 
+        public ActionResult UsersControl()
+        {
+            List<PageCommentsViewModel> list_pagecomments = unitOfWork.PageComments.GetAll().Select(x => new PageCommentsViewModel()
+            {
+                CommentID=x.CommentID,
+                PageTitel=x.Pages.Titel,
+                FullName=x.FullName,
+                Email=x.Email,
+                Comment=x.Comment,
+                IsActive=x.IsActive,
+                CreateDate=x.CreateDate
+            }).ToList();
+            return PartialView();
+        }
         protected override void Dispose(bool disposing)
         {
             unitOfWork.Dispose();
---
+++ PhoneTipProject/ViewModels/PageCommentsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneTipProject.ViewModels
+{
+    public class PageCommentsViewModel
+    {
+        [Display(Name = "شناسه کامنت")]
+        public int CommentID { get; set; }
+        [Display(Name = "عنوان خبر")]
+        public string PageTitel { get; set; }
+        [Display(Name = "نام کاربری")]
+        public string FullName { get; set; }
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+        [Display(Name = "نظرات")]
+        public string Comment { get; set; }
+        [Display(Name = "وضعیت")]
+        public bool IsActive { get; set; }
+        [Display(Name = "تاریخ ثبت")]
+        public DateTime CreateDate { get; set; }
+    }
+}
